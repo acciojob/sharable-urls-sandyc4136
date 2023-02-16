6517a2f2a58c367d42ace6bfeb17dda3b236e25b
Sandip Chhetry <sandyc4136@gmail.com>
2023-02-17 04:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // your code here
+var named=document.querySelector('#name');
+var form=document.querySelector('form');
+var year=document.querySelector('#year');
+var h3=document.querySelector('h3');
+var btn=document.querySelector('#button');
+
+
+function sharableURL(event){
+	event.preventDefault();
+	var h3Element="https://localhost:8080/";
+	if(named.value && year.value){
+		h3Element += "?name=" + named.value + "&?year=" + year.value;
+	}
+	else if(!named.value && year.value){
+		h3Element += "?year=" + year.value;
+	}
+	else if(named.value && !year.value){
+		h3Element += "?name=" + named.value;
+	}
+	h3.textContent=h3Element;
+}
+
+btn.addEventListener("submit",sharableURL);
+
+
+
+btn.addEventListener('click', sharableURL);
